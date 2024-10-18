52fdc28aa1fc2e5138308c7fd6ddb3f83978cd15
vnhatle <lenhat.bkit@gmail.com>
2024-10-17 19:14:12 -0500

    Update lab11

--- src/lesson11/lab/Q2BuildHeapBottomUp.java
+++ src/lesson11/lab/Q2BuildHeapBottomUp.java
@@ -5,28 +5,28 @@ import java.util.Arrays;
 public class Q2BuildHeapBottomUp {
 
     public static void main(String [] args) {
-        int [] arr = new int [] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+        int [] arr = new int [] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
         buildMaxHeapBottomUp(arr, arr.length);
         System.out.println("Build max heap bottom up: " + Arrays.toString(arr));
 
         heapSortPhaseTwo(arr, arr.length);
         System.out.println("Heap sort: " + Arrays.toString(arr));
 
-        int [] arr1 = new int [] {2, 1, 4, 3, 6, 5, 8, 7, 10, 9, 12, 11, 14, 13, 16, 15};
+        int [] arr1 = new int [] {0, 2, 1, 4, 3, 6, 5, 8, 7, 10, 9, 12, 11, 14, 13, 16, 15};
         buildMaxHeapBottomUp(arr1, arr1.length);
         System.out.println("Build max heap bottom up: " + Arrays.toString(arr1));
 
         heapSortPhaseTwo(arr1, arr1.length);
         System.out.println("Heap sort: " + Arrays.toString(arr1));
 
-        int [] arr2 = new int [] {4, 2, 3, 1, 5, 8, 7, 6, 11, 10, 12, 9, 13, 14, 16, 15};
+        int [] arr2 = new int [] {0, 4, 2, 3, 1, 5, 8, 7, 6, 11, 10, 12, 9, 13, 14, 16, 15};
         buildMaxHeapBottomUp(arr2, arr2.length);
         System.out.println("Build max heap bottom up: " + Arrays.toString(arr2));
 
         heapSortPhaseTwo(arr2, arr2.length);
         System.out.println("Heap sort: " + Arrays.toString(arr2));
 
-        int [] arr3 = new int [] {5, 6, 7, 4, 2, 3, 1, 8, 11, 13, 14, 16, 10, 12, 11, 9};
+        int [] arr3 = new int [] {0, 5, 6, 7, 4, 2, 3, 1, 8, 11, 13, 14, 16, 10, 12, 11, 9};
         buildMaxHeapBottomUp(arr3, arr3.length);
         System.out.println("Build max heap bottom up: " + Arrays.toString(arr3));
 
@@ -40,7 +40,7 @@ public class Q2BuildHeapBottomUp {
      * @param n
      */
     private static void buildMaxHeapBottomUp(int [] arr, int n) {
-        for (int i = n/2; i > 1; i--) {
+        for (int i = n/2; i >= 1; i--) {
             downHeap(arr, i, n);
         }
     }
@@ -63,11 +63,11 @@ public class Q2BuildHeapBottomUp {
 
     private static int maxChildIndex(int [] arr, int j, int n) {
         int k = j;
-        if (2 * j <= n && arr[2 * j] > arr[k]) {
+        if (2 * j < n && arr[2 * j] > arr[k]) {
             k = 2 * j;
         }
 
-        if (2 * j  + 1 <= n && arr[2 * j + 1] > arr[k]) {
+        if (2 * j  + 1 < n && arr[2 * j + 1] > arr[k]) {
             k = 2 * j + 1;
         }
 
@@ -81,7 +81,7 @@ public class Q2BuildHeapBottomUp {
      * @param n
      */
     private static void heapSortPhaseTwo(int [] arr, int n) {
-        for (int i = n; i >= 2; i--) {
+        for (int i = n - 1; i >= 2; i--) {
             swap(arr, 1, i);
             downHeap(arr, 1, i - 1);
         }
